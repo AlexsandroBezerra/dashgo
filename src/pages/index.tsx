030d06b60630a075ed6f11673ed7db350931f53f
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-04-13 19:49:35 -0300

    feat: add chakra

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <h1>Dashgo</h1>
+    </>
+  )
+}
