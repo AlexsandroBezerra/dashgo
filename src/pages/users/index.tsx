77c002d9f63eaccd4c295b909275cd96a83b2362
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-06-14 20:18:40 -0300

    feat: use axios in http requests

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,11 +8,11 @@ import { useQuery } from 'react-query'
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 import { Pagination } from '../../components/Pagination'
+import { api } from '../../services/api'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
+    const { data } = await api.get('users')
 
     const users = data.users.map(user => {
       return {
@@ -63,7 +63,10 @@ export default function UserList() {
 
           <Box flex="1" borderRadius={8} bg="gray.800" p="8">
             <Flex mb="8" justify="space-between" align="center">
-              <Heading size="lg" fontWeight="normal">Usuários</Heading>
+              <Heading size="lg" fontWeight="normal">
+                Usuários
+                {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+              </Heading>
 
               <Link href="/users/create" passHref>
                 <Button
